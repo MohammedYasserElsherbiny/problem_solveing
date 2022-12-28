e9aba54f2d720265d552a9ee20f8febe92d29439
MohammedYasserElsherbiny <mohammed.ys2003@gmail.com>
2022-12-28 22:30:35 +0200

    updatea

--- bank.cpp
+++ bank.cpp
@@ -17,10 +17,10 @@ using namespace std;
 
 
 
-
 class account_query
 {
-private:
+
+public:
     string account_number;
     string firstName;
     string lastName;
@@ -29,8 +29,8 @@ private:
     int limit;
     int pin;
     float total_Balance;
+    //vector<int> bill;
     //add password
-public:
     void read_data();
     void show_data(account_query temp);
     void write_rec();
@@ -39,15 +39,198 @@ public:
     void edit_rec();
     void delete_rec();
 };
-
-
 vector <account_query> vec;
+
+class bills : public account_query
+{
+    
+    public:
+    int n;
+    int b=rand();
+    int sumOfBills=0;
+    vector <int> BillsInAccount;
+    void GenerateBills ()
+    {
+        cout<<"\n Enter Record Number to see taxes: \n";
+        cin>>n;
+        b%=15;
+        if(b==0)
+        {
+            cout<<"\nYou Have No Bills!!\n";
+            return ;
+        }
+        for(int i=0;i<b;i++)
+        {
+            int tempNum=(rand()%1000)+1;
+            BillsInAccount.push_back(tempNum);
+            sumOfBills+=tempNum;
+        }
+        sort(BillsInAccount.begin(),BillsInAccount.end());
+    }
+
+    void ShowBills ()
+    {
+        cout<<"\n-------The Bills You Have-------\n";
+        GenerateBills();
+        for(int i=1;i<=b;i++)
+        {
+            cout<<"Bill Number "<<i<<": "<<BillsInAccount[i-1]<<'\n';
+        }
+    }
+
+    void PayBills ()
+    {
+        ShowBills();
+        cout<<"Do You Want To Pay Your Bills? \n";
+        string answer;
+        while (true)
+        {
+            cin>>answer;
+            for(int i=0;i<answer.size();i++)
+            {
+                answer[i]=towlower(answer[i]);
+            }
+            if(answer=="yes")
+            {
+                if(sumOfBills<=vec[n-1].total_Balance)
+                {
+                   total_Balance-=sumOfBills;
+                   cout<<"You Paied All Your Bills!! \n";
+                   cout<<"Your Total Balance Is "<<vec[n-1].total_Balance<<'\n'; 
+                }
+                else 
+                {
+                    int tempsum=0,cnt=0;
+                    for(int i=0;i<b;i++)
+                    {
+                        
+                        tempsum+=BillsInAccount[i];
+                        if(tempsum<=vec[n-1].total_Balance)
+                        {
+                            cnt++;
+                        }
+                        else
+                        {
+                            cout<<"You Can Pay "<<cnt<<" Bills.\n";
+                            cout<<"DO You Want To Pay Them?\n";
+                            cout<<"Yes/No\n";
+                            string str;
+                            here:
+                            cin>>str;
+                            for(int i=0;i<answer.size();i++)
+                            {
+                                str[i]=towlower(str[i]);
+                            }
+                            if(str=="no")
+                            {
+                                break;
+                            }
+                            else if(str=="yes")
+                            {
+                                vec[n-1].total_Balance-=tempsum;
+                            }
+                            else
+                            {
+                                cout<<"invaild input try again\n";
+                                goto here;
+                            }
+                        }
+                    }
+                }
+            }
+            else if(answer=="no")
+            {
+                cout<<"Ok Thanks For Your Visit \n";
+                break;
+            }
+            else
+            {
+                cout<<"Invalid Input Please Enter a Valid Input\nEthier YES or NO \n";
+            }
+        }
+        
+    }
+};
+
+class see_taxes :public bills
+{
+    private:
+	
+    int tempPin;
+    string tempAcc;
+		
+    public:
+
+    see_taxes()
+    {
+
+    }
+    void get()
+    {
+        int n;
+        cout<<"\n Enter Record Number to see taxes: \n";
+        cin>>n;
+        cout << "\nenter your pin then your account number : \n";
+        cin>>tempPin>>tempAcc;
+        if(tempPin==vec[n-1].pin&&tempAcc==vec[n-1].account_number)
+        {
+            if (vec[n-1].total_Balance != 0)
+            {
+                if (vec[n-1].total_Balance <= 50000)
+                {
+                    cout << "\ntaxes for this account is : \n" << (vec[n-1].total_Balance * 0.10);
+                }
+                else
+                {
+                    cout << "\ntaxes for this account is : \n" << (vec[n-1].total_Balance * 0.15);
+                }
+            }
+            else 
+            {
+                cout << "\nthere is no taxes for this account :)\n";
+            }
+        }
+        else
+        {
+            cout<<"incorect pin or account number \n";
+            cout<<"Do You Want to Try again? \n";
+            string tempanswer;
+            while (true)
+            {
+                cin>>tempanswer;
+                for(int i=0;i<tempanswer.size();i++)
+                {
+                    tempanswer[i]=tolower(tempanswer[i]);
+                }
+                if(tempanswer=="yes")
+                {
+                    get();
+                    break;
+                }
+                else if(tempanswer=="no")
+                {
+                    cout << "\n------thanks------\n" << endl;
+                    break;
+                }
+                else
+                {
+                    cout<<"invalid input try again\n";
+                }
+            }
+
+
+        }
+
+    }
+};
+
+
 
 
 
 void account_query::read_data()
 {
-    account_query temp;
+    see_taxes temp;
     string acc,f,l,cc,pn;
     int limit,pin;
     float t;
@@ -89,10 +272,10 @@ void account_query::show_data(account_query account)
     cout<<"Card Number: "<<account.credit_card<<endl;
     cout<<"-------------------------------"<<endl;
 }
-void account_query::write_rec()
-{
-    read_data();   
-}
+// void account_query::write_rec()
+// {
+//     read_data();   
+// }
 void account_query::read_rec()
 {
     
@@ -140,7 +323,7 @@ void account_query::edit_rec()
     cout<<"\nEnter data to Modify "<<endl;
     read_data();
 
-    vector<account_query> tempacc;
+    vector<bills> tempacc;
     for(int i=0;i<vec.size();i++)
     {
         if(i==n-1)
@@ -163,7 +346,7 @@ void account_query::delete_rec()
     cout<<"\n There are "<<count<<" record in the file";
     cout<<"\n Enter Record Number to Delete: ";
     cin>>n;
-    vector<account_query> tempacc;
+    vector<bills> tempacc;
     for(int i=0;i<vec.size();i++)
     {
         if(i==n-1)
@@ -179,7 +362,7 @@ void account_query::delete_rec()
 }
 int main()
 {
-    account_query A;
+    see_taxes A;
     int choice;
     cout<<"***Acount Information System***"<<endl;
     while(true)
@@ -196,7 +379,7 @@ int main()
         switch(choice)
         {
         case 1:
-            A.write_rec();
+            A.read_data();
             break;
         case 2:
             A.read_rec();
@@ -211,6 +394,11 @@ int main()
         case 5:
             A.delete_rec();
             break;
+        case 7:
+            A.get();
+            break;
+        case 8:
+            A.PayBills();
         case 6:
             exit(0);
             break;
