9334ba98ccbec4253bb489850450ba89aa29fbbd
MohammedYasserElsherbiny <mohammed.ys2003@gmail.com>
2023-01-05 20:38:19 +0200

    Second Order Statistics

---
+++ problem solving/CodeForces/A's/22A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define FIO ios_base::sync_with_stdio(false), cout.tie(nullptr); cin.tie(nullptr);
+using namespace std;
+
+
+
+
+int main ()
+{
+    FIO
+
+    int n;
+    cin>>n;
+    set<int> st;
+    for(int i=0;i<n;i++)
+    {
+       int temp;
+       cin>>temp;
+       st.insert(temp);
+    }
+    auto it= st.begin();
+    it++;
+    if(st.size()==1)
+    {
+        cout<<"NO";
+        return 0;
+    }
+    cout<<*it;
+}
